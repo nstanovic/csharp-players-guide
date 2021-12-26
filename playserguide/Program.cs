75c5e37f80d4958df3dd3fb32d039057d6007d77
Nick Stanovic <nickstanovic@#gmail.com>
2021-12-26 00:30:47 -0500

    complete bread exercise

--- playserguide/Program.cs
+++ playserguide/Program.cs
@@ -8,3 +8,9 @@
 // Console.WriteLine("What's up?");
 // Console.WriteLine("I'm good.");
 // Console.WriteLine("That's great!");
+
+// ask who wants bread and then give them a bread
+Console.WriteLine("Bread is ready!");
+Console.WriteLine("Who wants bread?");
+string breadRecipient = Console.ReadLine();
+Console.WriteLine($"{breadRecipient} got bread.");
