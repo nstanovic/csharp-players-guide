a498a4f3a0dd41448ec0b7efee60b4f3a0dfe552
Nick Stanovic <nickstanovic@#gmail.com>
2021-12-26 00:11:14 -0500

    add more statements

--- playserguide/Program.cs
+++ playserguide/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, Nick!");
+
+// make 5 WriteLine statements
+Console.WriteLine("Hey there!");
+Console.WriteLine("How are you?");
+Console.WriteLine("What's up?");
+Console.WriteLine("I'm good.");
+Console.WriteLine("That's great!");
