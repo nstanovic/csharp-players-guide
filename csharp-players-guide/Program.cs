c42f1bc5bb495c9543862e5b1c75f59f55da184c
Nick Stanovic <nickstanovic@gmail.com>
2022-01-01 10:49:31 -0500

    complete part 4

--- csharp-players-guide/Program.cs
+++ csharp-players-guide/Program.cs
@@ -378,7 +378,8 @@ for (int shots=1; shots < 101; shots++)
     else if (shots % 5 == 0)
         Console.WriteLine("Electric Blast");
 // part 4. if statement to indicate fiery electric blast if both parts 2 and 3 are true
-
+    else if (shots % 15 == 0)
+        Console.WriteLine("Fiery Electric Blast");
 
 // part 5. else statement to indicate normal shot on all other turns
 
